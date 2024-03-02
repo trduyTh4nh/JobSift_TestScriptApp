7fbfb954c7f42c9a088d3a40b73587592e8d26d5
Duy Thanh <tttranduy999@gmail.com>
2024-03-02 10:08:35 +0700

    thêm excel

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { remote } = require('webdriverio');
+const XLSX = require('xlsx');
 
 const capabilities = {
   platformName: 'Android',
@@ -20,14 +21,31 @@ async function runTest() {
   const driver = await remote(wdOpts);
   try {
     await driver.pause(14000)
+    // đọc dữ liệu từ Excel 
+    const workbook = XLSX.readFile('D:\\DBCLPM_JOBSIFT.xlsx')
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+
+    const cellF4Value = sheet['F4'] ? sheet['F4'].v : null;
+
+    const cellF5Value = sheet['F5'] ? sheet['F5'].v : null;
+
+    console.log(cellF4Value)
+    console.log(cellF5Value)
+
+
+
+
+
+
+
     var btnLogin = await driver.$('//android.view.ViewGroup[@content-desc="Login"]')
     btnLogin.click();
 
   } finally {
-    
+
   }
 }
 
 
 runTest().catch(console.error);
- 

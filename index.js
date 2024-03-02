382a0df47a3a60fd85593115e08ed7ecc980b915
Duy Thanh <tttranduy999@gmail.com>
2024-03-02 10:47:04 +0700

    lụm

--- index.js
+++ index.js
@@ -20,27 +20,7 @@ const wdOpts = {
 async function runTest() {
   const driver = await remote(wdOpts);
   try {
-    await driver.pause(14000)
-    // đọc dữ liệu từ Excel 
-    const workbook = XLSX.readFile('D:\\DBCLPM_JOBSIFT.xlsx')
-    const sheetName = workbook.SheetNames[0];
-    const sheet = workbook.Sheets[sheetName];
-
-    const cellF4Value = sheet['F4'] ? sheet['F4'].v : null;
-
-    const cellF5Value = sheet['F5'] ? sheet['F5'].v : null;
-
-    console.log(cellF4Value)
-    console.log(cellF5Value)
-
-
-
-
-
-
-
-    var btnLogin = await driver.$('//android.view.ViewGroup[@content-desc="Login"]')
-    btnLogin.click();
+    
 
   } finally {
 
